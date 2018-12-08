8eba721c82d6c728fff85e9cb08245227b8ea99b
SashaPorubaymikh <sasha095009@gmail.com>
2018-12-08 14:09:00 +0200

    Заново

--- Tanks.py
+++ Tanks.py
@@ -1,26 +1,5 @@
 import pygame
 from DisplayScale import screen_width, screen_height
-from Characters import Player
-from Blocks import Block
-from Levels import level1
-
-win = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-screen = pygame.Surface((screen_width, screen_height))
-
-sprite_group = pygame.sprite.Group()
-bricks_group = []
-
-x = y = 0
-for row in level1:
-    for col in row:
-        if col == '0':
-            b1 = Block(x, y, 'images/blocks/brick.png')
-            sprite_group.add(b1)
-            bricks_group.append(b1)
-        x += 40
-    y += 40
-    x = 0
-
 
 done = True
 clock = pygame.time.Clock()
@@ -32,6 +11,10 @@ while done:
     if e.type == pygame.KEYDOWN:
         if e.key == pygame.ESCAPE:
             done = False
+
+    for i in sprite_group:
+        screen.blit(i.bitmap, (i.x, i.y))
+    screen.fill((150, 150, 150))
 
     win.blit(screen, (0, 0))
     
