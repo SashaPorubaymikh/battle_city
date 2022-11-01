b5b0e6126eb778f748ad80d6987139ea4aaa78fc
Oleksandr Porubaimikh <37808044+llem00n@users.noreply.github.com>
2022-11-01 11:00:27 +0200

    fix levels

--- Levels.py
+++ Levels.py
@@ -5,7 +5,6 @@ flevel4 = open('levels/level_4', 'r')
 flevel5 = open('levels/level_5', 'r')
 flevel6 = open('levels/level_6', 'r')
 flevel7 = open('levels/level_7', 'r')
-flevel8 = open('levels/level_8', 'r')
 
 level1 = []
 level2 = []
@@ -14,7 +13,6 @@ level4 = []
 level5 = []
 level6 = []
 level7 = []
-level8 = []
 def mklv(file, list):
     for line in file:
         list.append(line)
